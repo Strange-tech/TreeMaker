7269bf5988078ba6abc18d57b2cf2bcbe9d1e1f6
luziang <939543598@qq.com>
2023-01-15 00:03:38 +0800

    部署到netlify

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 import * as THREE from "three";
 import { Mesh } from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 function main() {
   const canvas = document.querySelector("#c");
